f85f131b0e457868f56513948e0a7a14e443016d
parkoon <par_koon@naver.com>
2019-03-06 18:33:33 +0900

    기본 React 구조 세팅

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+const React, { Component } = require('react')
+
+class App extends Component {
+
+    render() {
+        return (
+            <div>
+                Hello React
+            </div>
+        )
+    }
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,6 @@
+const ReactDOM = require('react-dom')
+const App = require('./components/App')
+
+require('./index.css')
+
+ReactDOM.render(<App />, document.getElementById('root'))
